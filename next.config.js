de94aa99a94faf60a1399824197e0692a794dc52
sirikancharla12 <sirikancharla1290@gmail.com>
2026-01-01 18:11:54 +0530

    package.json

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   eslint: {
-    ignoreDuringBuilds: true,
-  },
   reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   images: {
     domains: ["lh3.googleusercontent.com"],
-  },
-   typescript: {
-    ignoreBuildErrors: true, 
   },
 };
 
